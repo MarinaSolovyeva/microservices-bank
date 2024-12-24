83ffbe66dfdeba26308ee0d8ec8687f07b3f53b4
marina <marinasolovieva189@gmail.com>
2024-12-24 18:00:04 +0100

    add section with mysql db

---
+++ section7/accounts/src/main/java/com/solovieva/accounts/mapper/CustomerMapper.java
@@ -0,0 +1,22 @@
+package com.solovieva.accounts.mapper;
+
+import com.solovieva.accounts.dto.CustomerDto;
+import com.solovieva.accounts.entity.Customer;
+
+public class CustomerMapper {
+
+    public static CustomerDto mapToCustomerDto(Customer customer, CustomerDto customerDto) {
+        customerDto.setName(customer.getName());
+        customerDto.setEmail(customer.getEmail());
+        customerDto.setMobileNumber(customer.getMobileNumber());
+        return customerDto;
+    }
+
+    public static Customer mapToCustomer(CustomerDto customerDto, Customer customer) {
+        customer.setName(customerDto.getName());
+        customer.setEmail(customerDto.getEmail());
+        customer.setMobileNumber(customerDto.getMobileNumber());
+        return customer;
+    }
+
+}
