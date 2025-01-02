a59d2538cfe299cb77cfeb152bca5ea60a9f7b1f
marina <marinasolovieva189@gmail.com>
2025-01-02 21:24:29 +0100

    imlement resilient patterns

---
+++ section_10/accounts/src/main/java/com/solovieva/accounts/mapper/AccountsMapper.java
@@ -0,0 +1,22 @@
+package com.solovieva.accounts.mapper;
+
+import com.solovieva.accounts.dto.AccountsDto;
+import com.solovieva.accounts.entity.Accounts;
+
+public class AccountsMapper {
+
+    public static AccountsDto mapToAccountsDto(Accounts accounts, AccountsDto accountsDto) {
+        accountsDto.setAccountNumber(accounts.getAccountNumber());
+        accountsDto.setAccountType(accounts.getAccountType());
+        accountsDto.setBranchAddress(accounts.getBranchAddress());
+        return accountsDto;
+    }
+
+    public static Accounts mapToAccounts(AccountsDto accountsDto, Accounts accounts) {
+        accounts.setAccountNumber(accountsDto.getAccountNumber());
+        accounts.setAccountType(accountsDto.getAccountType());
+        accounts.setBranchAddress(accountsDto.getBranchAddress());
+        return accounts;
+    }
+
+}
