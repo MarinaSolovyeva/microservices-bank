0fa97ee5c93ea7c0d1db9798be9740ebdb7959ae
marina <marinasolovieva189@gmail.com>
2025-01-06 01:11:22 +0100

    fixes

--- section_11/cards/src/main/java/com/solovieva/cards/controller/CardsController.java
+++ section_11/cards/src/main/java/com/solovieva/cards/controller/CardsController.java
@@ -100,10 +100,11 @@ public class CardsController {
     @GetMapping("/fetch")
     public ResponseEntity<CardsDto> fetchCardDetails(@RequestHeader("eazybank-correlation-id") String correlationId,
                                                      @RequestParam
-                                                               @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
-                                                               String mobileNumber) {
-        logger.debug("eazyBank-correlation-id found: {} ", correlationId);
+                                                     @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
+                                                     String mobileNumber) {
+        logger.debug("fetchCardDetails method start");
         CardsDto cardsDto = iCardsService.fetchCard(mobileNumber);
+        logger.debug("fetchCardDetails method end");
         return ResponseEntity.status(HttpStatus.OK).body(cardsDto);
     }
 
