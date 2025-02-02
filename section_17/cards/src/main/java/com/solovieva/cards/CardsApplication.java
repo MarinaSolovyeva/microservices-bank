b903a9e3e75014598eed49828c125525d9a4a897
marina <marinasolovieva189@gmail.com>
2025-02-02 11:44:11 +0100

    disctovery and lb using kuber

---
+++ section_17/cards/src/main/java/com/solovieva/cards/CardsApplication.java
@@ -0,0 +1,47 @@
+package com.solovieva.cards;
+
+import com.solovieva.cards.dto.CardsContactInfoDto;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+/*@ComponentScans({ @ComponentScan("com.solovieva.cards.controller") })
+@EnableJpaRepositories("com.solovieva.cards.repository")
+@EntityScan("com.solovieva.cards.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {CardsContactInfoDto.class})
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Cards microservice REST API Documentation",
+				description = "EazyBank Cards microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Madan Reddy",
+						email = "tutor@solovieva.com",
+						url = "https://www.solovieva.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.solovieva.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "EazyBank Cards microservice REST API Documentation",
+				url = "https://www.solovieva.com/swagger-ui.html"
+		)
+)
+public class CardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsApplication.class, args);
+	}
+}
